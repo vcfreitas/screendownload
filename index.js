54c67e7ff62c557c80542178e7fe37bebad5add6
Samuel de Padua Faria <saamuel.faria@gmail.com>
2016-03-28 18:24:57 -0300

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var express = require('express');
+var app = (function(){
+    var app = express();
+
+    //Configuração de Ambiante
+    app.set('port', 80);
+
+    // Middleware
+    app.use(express.static('public'));
+
+
+    return app;
+})();
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express Server escutando na porta ' + app.get('port'));
+});
